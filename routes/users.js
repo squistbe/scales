8bd0e36bd8fd181bdb28908459fdb2769f4a8ae9
spencer.quistberg <spencer.quistberg@remedyinformatics.com>
2014-11-10 14:21:52 -0700

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+// Create
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    db.collection('userlist').insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+// Read
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    db.collection('userlist').find().toArray(function (err, items) {
+        res.json(items);
+    });
+});
+
+// Delete
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var userToDelete = req.params.id;
+    db.collection('userlist').removeById(userToDelete, function(err, result) {
+        res.send((result === 1) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
+
+module.exports = router;

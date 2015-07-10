49f8c8bed7565ee608da759978ce2e4d90bd3feb
Spencer Quistberg <quistber@adobe.com>
2015-07-10 09:23:17 -0600

    bower post install

--- public/app/users/userController.js
+++ public/app/users/userController.js
@@ -6,14 +6,5 @@
     Users.$inject = ['$scope', 'UsersService'];
     function Users($scope, UsersService) {
     	$scope.userList = UsersService.query();
-    	$scope.getRandomUrl = getRandomUrl;
-
-    	function getRandomUrl() {
-    		var randomNumber = Math.random();
-
-    		if (randomNumber < .33) return 'http://fillmurray.com/100/100';
-    		else if (randomNumber > .66) return 'http://www.placecage.com/100/100';
-    		else return 'http://stevensegallery.com/100/100';
-    	}
     }
 })();
